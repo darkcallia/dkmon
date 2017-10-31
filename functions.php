febaea814ec000466805dcef1f9bf27e65780bc3
darkcallia <xcallia@gmail.com>
2017-10-31 23:45:21 +0300

    Возвращен стиль elementfoto и datfoto

--- functions.php
+++ functions.php
@@ -206,8 +206,8 @@ function service_graph($source, $title, $comment)
  /* вывод графика -----------*/
  if ( false!==file("$source") )
  {
-  echo ("<div class='element'><div class='dat' style='background:rgb(160,203,169);background:linear-gradient(rgb(160,203,169), rgb(99,169,113));' ");
-  echo (" data-title=''><img width=40% height=100% src='$source' onclick='javascript:document.location.href=\"$source\"'>");
+  echo ("<div class='elementfoto'><div class='datfoto' style='background:rgb(160,203,169);background:linear-gradient(rgb(160,203,169), rgb(99,169,113));' ");
+  echo (" data-title=''><img height=100% src='$source' onclick='javascript:document.location.href=\"$source\"'>");
   echo ("</div><div class='text'>$title");
   echo ("</div><div class='note'>");
   echo ("</div></div>");
